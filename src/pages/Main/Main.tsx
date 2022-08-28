47342cd9261a60c4c8e3e94223773f4b4fe9efa5
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-28 15:07:23 +0530

    add: added support for chakra

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import {useRoutes} from 'react-router-dom'
 import styles from "./Main.module.scss";
 
 const Main: React.FC = () => {
-  return <div>Main</div>;
+  return <div className={styles.MainContainer}>
+    <section className={styles.SearchContainer}></section>
+    <section className={styles.PokemonContainer}></section>
+  </div>;
 };
 
 export default Main;
