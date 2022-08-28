b293697d20676a88978ca6ca1183882160d276df
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-28 15:13:48 +0530

    add: added support for react-icons

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,42 @@
 import React from "react";
-import {useRoutes} from 'react-router-dom'
+import {useRoutes} from 'react-router-dom';
+import { BsSearch } from "react-icons/bs";
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Spinner,
+  useToast,
+} from "@chakra-ui/react";
+
 import styles from "./Main.module.scss";
 
 const Main: React.FC = () => {
+   const [searchInput, setSearchInput] = React.useState<string>("");
+
+  const handleSearch = () => {}
+
   return <div className={styles.MainContainer}>
-    <section className={styles.SearchContainer}></section>
+    <section className={styles.SearchContainer}>
+      <InputGroup>
+            <InputLeftElement pointerEvents="none" children={<BsSearch />} />
+            <Input
+              type="text"
+              placeholder="news title"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </InputGroup>
+          <Button
+            className={styles.Button}
+            colorScheme="green"
+            variant="solid"
+            onClick={handleSearch}
+          >
+            Search
+          </Button>
+    </section>
     <section className={styles.PokemonContainer}></section>
   </div>;
 };
