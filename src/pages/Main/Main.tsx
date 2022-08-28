2003fd08de230c3e5a9ced5126fe3a1465038965
Suranjan <suranjanbiswas2002@gmail.com>
2022-08-28 16:12:19 +0530

    add: added query for search

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -14,29 +14,35 @@ import styles from "./Main.module.scss";
 
 const Main: React.FC = () => {
   const [searchInput, setSearchInput] = React.useState<string>("");
+  const [suggestedData, setSuggestedData] = React.useState<
+    { name: string; url: string }[]
+  >([]);
 
   const handleSearch = () => {};
 
   return (
     <div className={styles.MainContainer}>
       <section className={styles.SearchContainer}>
-        <InputGroup>
-          <InputLeftElement pointerEvents="none" children={<BsSearch />} />
-          <Input
-            type="text"
-            placeholder="pokemon name"
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-          />
-        </InputGroup>
-        <Button
-          className={styles.Button}
-          colorScheme="green"
-          variant="solid"
-          onClick={handleSearch}
-        >
-          Search
-        </Button>
+        <div className={styles.Inputs}>
+          <InputGroup>
+            <InputLeftElement pointerEvents="none" children={<BsSearch />} />
+            <Input
+              type="text"
+              placeholder="pokemon name"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </InputGroup>
+          <Button
+            className={styles.Button}
+            colorScheme="green"
+            variant="solid"
+            onClick={handleSearch}
+          >
+            Search
+          </Button>
+        </div>
+        {true && <div className={styles.SuggestionModals}></div>}
       </section>
       <section className={styles.PokemonContainer}></section>
     </div>
